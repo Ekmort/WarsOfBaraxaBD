a34ee8500c7bbe6d2390f60cb74deeda835998aa
Ekmort <ydmule@hotmail.com>
2014-10-08 15:13:11 -0400

    ListerDeckJoueur Fini

--- WarsOfBaraxaBD/AccesBD.cs
+++ WarsOfBaraxaBD/AccesBD.cs
@@ -10,25 +10,28 @@ namespace WarsOfBaraxaBD
 {
     public class AccesBD
     {
-        public OracleConnection conn;
-        public String connexionChaine;
+        private OracleConnection conn;
+        private String connexionChaine;
         public AccesBD()
         {
-           String serveur = "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.17.104.113)"
-           + "(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = orcl)))";
-           connexionChaine = "data source=" + serveur + ";user id=WarsOfBaraxa;password=WarsOfBaraxa";
+           String serveur = "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.17.104.127)"
+           + "(PORT = 1522)))(CONNECT_DATA =(SERVICE_NAME = WarsOfBaraxa)))";
+           connexionChaine = "data source=" + serveur + ";userid=WarsOfBaraxa;password=WarsOfBaraxa";
         }
 
         public void Connection()
         {
-            conn = new OracleConnection();
+            conn = new OracleConnection(connexionChaine);
             conn.Open();
         }
 
-        public void ListerDeckJoueur(String NomJoueur)
+        public Carte[] ListerDeckJoueur(String NomJoueur,int NoDeck)
         {
             Carte []CarteJoueur = null;
-            string sql = "SELECT * FROM DECKJOUEUR WHERE IdJoueur=" + NomJoueur;
+            string sql = "SELECT NomCarte,TypeCarte,Habilete,Ble,Bois,Gem,C.NoCarte FROM CARTE C " +
+            "INNER JOIN DeckCarte CD ON C.NoCarte=CD.NoCarte " +
+            "INNER JOIN DECK D ON CD.NoDeck=D.NoDeck " +
+            "INNER JOIN DECKJOUEUR DJ ON D.NoDeck=DJ.NoDeck WHERE DJ.IdJoueur=" + NomJoueur + " AND DJ.NoDeck=" + NoDeck;
             OracleCommand commandeOracle = new OracleCommand(sql,conn);
             OracleDataReader dataReader = commandeOracle.ExecuteReader();
 
@@ -36,10 +39,17 @@ namespace WarsOfBaraxaBD
             {
                 for(int i=0;dataReader.Read();++i)
                 {
-                    CarteJoueur[i] = new Carte(dataReader.GetString(0),dataReader.GetString(1),dataReader.GetInt32(2),dataReader.GetInt32(3),dataReader.GetInt32(4));
+                    CarteJoueur[i] = new Carte(dataReader.GetString(0),dataReader.GetString(1),dataReader.GetString(2),dataReader.GetInt32(3),dataReader.GetInt32(4),dataReader.GetInt32(5));
+                    if(dataReader.GetString(1) == "Permanents")
+                    {
+                        string sqlPerm = "SELECT TypePerm,Attaque,Armure,Vie FROM Permanents WHERE NoCarte=" + dataReader.GetInt32(5);
+                        OracleCommand commandeOraclePerm = new OracleCommand(sqlPerm,conn);
+                        OracleDataReader dataReaderPerm = commandeOraclePerm.ExecuteReader();
+                        CarteJoueur[i].perm = new Permanent(dataReaderPerm.GetString(0),dataReaderPerm.GetInt32(1),dataReaderPerm.GetInt32(2),dataReaderPerm.GetInt32(3));
+                    }
                 }
             }
-            
+            return CarteJoueur;
         }
     }
 }
