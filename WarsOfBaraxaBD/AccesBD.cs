bb1e234ae9cba1d726cbd129ea0b449c87a0d22b
Ekmort <ydmule@hotmail.com>
2014-10-09 12:04:14 -0400

    BD Marche

--- WarsOfBaraxaBD/AccesBD.cs
+++ WarsOfBaraxaBD/AccesBD.cs
@@ -10,12 +10,12 @@ namespace WarsOfBaraxaBD
 {
     public class AccesBD
     {
-        private OracleConnection conn;
-        private String connexionChaine;
+        static private OracleConnection conn;
+        static private String connexionChaine;
         public AccesBD()
         {
             String serveur = "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 172.17.104.127)"
-            + "(PORT = 1522)))(CONNECT_DATA =(SERVICE_NAME = WarsOfBaraxa)))";
+            + "(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = ORCL)))";
             connexionChaine = "data source=" + serveur + ";userid=WarsOfBaraxa;password=WarsOfBaraxa";
         }
 
@@ -25,34 +25,42 @@ namespace WarsOfBaraxaBD
             conn.Open();
         }
 
-        public Carte[] ListerDeckJoueur(String NomJoueur, int NoDeck)
+        static public Carte[] ListerDeckJoueur(String NomJoueur, int NoDeck)
         {
-            Carte[] CarteJoueur = null;
-            string sql = "SELECT NomCarte,TypeCarte,Habilete,Ble,Bois,Gem,C.NoCarte,NombreDeFois FROM CARTE C " +
+            Carte[] CarteJoueur = new Carte[40];
+            string sql = "SELECT NomCarte,TypeCarte,NVL(Habilete,'null'),Ble,Bois,Gem,C.NoCarte,NombreDeFois FROM CARTE C " +
             "INNER JOIN DeckCarte CD ON C.NoCarte=CD.NoCarte " +
             "INNER JOIN DECK D ON CD.NoDeck=D.NoDeck " +
-            "INNER JOIN DECKJOUEUR DJ ON D.NoDeck=DJ.NoDeck WHERE DJ.IdJoueur=" + NomJoueur + " AND DJ.NoDeck=" + NoDeck;
+            "INNER JOIN DECKJOUEUR DJ ON D.NoDeck=DJ.NoDeck WHERE DJ.IdJoueur='" + NomJoueur + "' AND DJ.NoDeck=" + NoDeck;
             OracleCommand commandeOracle = new OracleCommand(sql, conn);
-            OracleDataReader dataReader = commandeOracle.ExecuteReader();
 
-            if (dataReader.HasRows)
+            try
             {
-                while (dataReader.Read())
+                OracleDataReader dataReader = commandeOracle.ExecuteReader();
+
+                if (dataReader.HasRows)
                 {
                     int i = 0;
-                    for (int j = 0; j < dataReader.GetInt32(7); ++j)
+                    while (dataReader.Read())
                     {
-                        CarteJoueur[i] = new Carte(dataReader.GetString(0), dataReader.GetString(1), dataReader.GetString(2), dataReader.GetInt32(3), dataReader.GetInt32(4), dataReader.GetInt32(5));
-                        if (dataReader.GetString(1) == "Permanents")
+                        for (int j = 0; j < dataReader.GetInt32(7); ++j)
                         {
-                            string sqlPerm = "SELECT TypePerm,Attaque,Armure,Vie FROM Permanents WHERE NoCarte=" + dataReader.GetInt32(6);
-                            OracleCommand commandeOraclePerm = new OracleCommand(sqlPerm, conn);
-                            OracleDataReader dataReaderPerm = commandeOraclePerm.ExecuteReader();
-                            CarteJoueur[i].perm = new Permanent(dataReaderPerm.GetString(0), dataReaderPerm.GetInt32(1), dataReaderPerm.GetInt32(2), dataReaderPerm.GetInt32(3));
+                            CarteJoueur[i] = new Carte(dataReader.GetString(0), dataReader.GetString(1), dataReader.GetString(2), dataReader.GetInt32(3), dataReader.GetInt32(4), dataReader.GetInt32(5));
+                            if (dataReader.GetString(1) == "Permanents")
+                            {
+                                string sqlPerm = "SELECT TypePerm,Attaque,Armure,Vie FROM Permanents WHERE NoCarte=" + dataReader.GetInt32(6);
+                                OracleCommand commandeOraclePerm = new OracleCommand(sqlPerm, conn);
+                                OracleDataReader dataReaderPerm = commandeOraclePerm.ExecuteReader();
+                                CarteJoueur[i].perm = new Permanent(dataReaderPerm.GetString(0), dataReaderPerm.GetInt32(1), dataReaderPerm.GetInt32(2), dataReaderPerm.GetInt32(3));
+                            }
+                            ++i;
                         }
-                        ++i;
                     }
                 }
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.Write(e);
             }
             return CarteJoueur;
         }
